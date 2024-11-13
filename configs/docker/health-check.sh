d07915906e5f579e5a8f8f4667abbd04fa8558ae
Drew Brokke <drew.brokke@liferay.com>
2024-11-13 15:01:26 -0600

    adds health check file for the liferay service

---
+++ configs/docker/health-check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function _log() {
+	echo "[$(date "+%Y.%m.%d %H:%M:%S")] $1" >> health-check.log
+}
+
+_log "Checking for license registration..."
+if ! grep "License registered for DXP Development" logs/liferay.*.log
+then
+	_log "License not registered"
+	exit 1
+fi
+
+_log "Waiting for the server to be reachable..."
+if ! curl localhost:8080
+then
+	_log "Server not reachable"
+	exit 1
+fi
+
+_log "Ready!"
+exit 0
