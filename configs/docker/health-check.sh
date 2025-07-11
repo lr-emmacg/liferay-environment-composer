9278b84ff4d0e6542490b83b9deb96993646fad8
Drew Brokke <drew.brokke@liferay.com>
2025-07-11 17:21:12 -0500

    LPD-58367 health-check.sh: shfmt

--- configs/docker/health-check.sh
+++ configs/docker/health-check.sh
@@ -4,19 +4,16 @@ function _log() {
 	echo "[$(date "+%Y.%m.%d %H:%M:%S")] $1" | tee -a health-check.log
 }
 
-if ! grep -i -E "ga" .liferay-version
-then
+if ! grep -i -E "ga" .liferay-version; then
 	_log "Checking for license registration..."
-	if ! grep -i -E "license validation passed" logs/liferay.*.log
-	then
+	if ! grep -i -E "license validation passed" logs/liferay.*.log; then
 		_log "License not registered"
 		exit 1
 	fi
 fi
 
 _log "Waiting for the server to be reachable..."
-if ! curl -s localhost:8080 -o /dev/null --write-out "%{http_code}"
-then
+if ! curl -s localhost:8080 -o /dev/null --write-out "%{http_code}"; then
 	_log "Server not reachable"
 	exit 1
 fi
