3fcc117289a3e46b5ea15ce3f1d5f6315468d6ab
Anthony Chu <anthony.chu@liferay.com>
2025-08-07 09:11:43 -0700

    LPD-62327 Use mounted dumps directory to fetch backups

--- compose-recipes/db2/var_custom/restore-lportal.sh
+++ compose-recipes/db2/var_custom/restore-lportal.sh
@@ -1,6 +1,6 @@
 echo "lportal" | su db2admin <<EOSU
 . /database/config/db2admin/sqllib/db2profile
-[ "$(ls /database/data/db2admin/NODE0000/lportal/backup)" ] && \
+[ "$(ls /database/data/db2admin/NODE0000/backups)" ] && \
 db2 connect to lportal && \
 db2 force application all && \
 db2 terminate && \
@@ -8,7 +8,7 @@ db2stop force && \
 ipclean -a && \
 db2set -null DB2COMM && \
 db2start admin mode restricted access && \
-db2 RESTORE DATABASE lportal FROM /database/data/db2admin/NODE0000/lportal/backup INTO lportal REPLACE EXISTING WITHOUT ROLLING FORWARD && \
+db2 RESTORE DATABASE lportal FROM /database/data/db2admin/NODE0000/backups INTO lportal REPLACE EXISTING WITHOUT ROLLING FORWARD && \
 db2stop force && \
 ipclean -a && \
 db2set DB2COMM=TCPIP && \
