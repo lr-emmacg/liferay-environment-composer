108b1a0a185ac7746cfb32c38307a896d9f33263
Anthony Chu <anthony.chu@liferay.com>
2025-08-07 09:13:44 -0700

    LPD-62327 Paramaterize custom scripts

--- compose-recipes/db2/var_custom/create-lportal.sh
+++ compose-recipes/db2/var_custom/create-lportal.sh
@@ -1,4 +1,4 @@
-echo "lportal" | su db2admin <<EOSU
-. /database/config/db2admin/sqllib/db2profile
-db2 create db lportal pagesize 32768 temporary tablespace managed by automatic storage
+echo ${DB2INST1_PASSWORD} | su ${DB2INSTANCE} <<EOSU
+. /database/config/${DB2INSTANCE}/sqllib/db2profile
+db2 create db ${COMPOSER_DATABASE_NAME} pagesize 32768 temporary tablespace managed by automatic storage
 EOSU
--- compose-recipes/db2/var_custom/restore-lportal.sh
+++ compose-recipes/db2/var_custom/restore-lportal.sh
@@ -1,17 +1,17 @@
-echo "lportal" | su db2admin <<EOSU
-. /database/config/db2admin/sqllib/db2profile
-[ "$(ls /database/data/db2admin/NODE0000/backups)" ] && \
-db2 connect to lportal && \
+echo "${DB2INST1_PASSWORD}" | su ${DB2INSTANCE} <<EOSU
+. /database/config/${DB2INSTANCE}/sqllib/db2profile
+[ "$(ls /database/data/${DB2INSTANCE}/NODE0000/backups)" ] && \
+db2 connect to ${COMPOSER_DATABASE_NAME} && \
 db2 force application all && \
 db2 terminate && \
 db2stop force && \
 ipclean -a && \
 db2set -null DB2COMM && \
 db2start admin mode restricted access && \
-db2 RESTORE DATABASE lportal FROM /database/data/db2admin/NODE0000/backups INTO lportal REPLACE EXISTING WITHOUT ROLLING FORWARD && \
+db2 RESTORE DATABASE ${COMPOSER_DATABASE_NAME} FROM /database/data/${DB2INSTANCE}/NODE0000/backups INTO ${COMPOSER_DATABASE_NAME} REPLACE EXISTING WITHOUT ROLLING FORWARD && \
 db2stop force && \
 ipclean -a && \
 db2set DB2COMM=TCPIP && \
 db2start && \
-db2 activate db lportal || echo
+db2 activate db ${COMPOSER_DATABASE_NAME} || echo
 EOSU
