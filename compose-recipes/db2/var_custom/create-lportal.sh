60f08d185899c70f4550e618f7f94e95ae91c740
Anthony Chu <anthony.chu@liferay.com>
2025-08-07 11:21:21 -0700

    LPD-62327 Only create database if it does not already exist

--- compose-recipes/db2/var_custom/create-lportal.sh
+++ compose-recipes/db2/var_custom/create-lportal.sh
@@ -1,4 +1,4 @@
 echo ${DB2INST1_PASSWORD} | su ${DB2INSTANCE} <<EOSU
 . /database/config/${DB2INSTANCE}/sqllib/db2profile
-db2 create db ${COMPOSER_DATABASE_NAME} pagesize 32768 temporary tablespace managed by automatic storage
+[ ! -d /database/data/${DB2INSTANCE}/NODE0000/${COMPOSER_DATABASE_NAME^^} ] && db2 create db ${COMPOSER_DATABASE_NAME} pagesize 32768 temporary tablespace managed by automatic storage || echo
 EOSU
