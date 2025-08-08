083d507a85a62ce08d40ce2bd162c0c77b0367cb
Anthony Chu <anthony.chu@liferay.com>
2025-08-08 13:49:02 -0700

    LPD-62327 Split lines to increase readability

--- compose-recipes/db2/var_custom/create-database.sh
+++ compose-recipes/db2/var_custom/create-database.sh
@@ -1,4 +1,6 @@
 echo ${DB2INST1_PASSWORD} | su ${DB2INSTANCE} <<EOSU
 . /database/config/${DB2INSTANCE}/sqllib/db2profile
-[ ! -d /database/data/${DB2INSTANCE}/NODE0000/${COMPOSER_DATABASE_NAME^^} ] && db2 create db ${COMPOSER_DATABASE_NAME} pagesize 32768 temporary tablespace managed by automatic storage || echo
+[ ! -d /database/data/${DB2INSTANCE}/NODE0000/${COMPOSER_DATABASE_NAME^^} ] && \
+db2 create db ${COMPOSER_DATABASE_NAME} pagesize 32768 temporary tablespace managed by automatic storage || \
+echo
 EOSU
