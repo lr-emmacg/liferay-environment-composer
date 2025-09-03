3278c333553ed5a85f73df27cb1e722673d07fcd
Drew Brokke <drew.brokke@liferay.com>
2025-09-03 10:10:21 -0500

    LPD-64401 adds shell-source.sh to be sourced by shells to install the script as an alias

---
+++ scripts/cli/shell-source.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+LEC_REPO_ROOT="${LIFERAY_ENVIRONMENT_COMPOSER_HOME:?The LIFERAY_ENVIRONMENT_COMPOSER_HOME environment variable must be set}"
+
+# Set the alias to the main script
+# shellcheck disable=SC2139
+alias lec="$LEC_REPO_ROOT/scripts/cli/lec.sh"
+
+# Util function to allow quickly jumping to a project
+function lecd() {
+	local worktree_name="$*"
+
+	local worktree_dir
+	worktree_dir="$(
+		lec list |
+			fzf \
+				--delimiter "/" \
+				--exit-0 \
+				--height "50%" \
+				--no-multi \
+				--nth "-1" \
+				--query "${worktree_name}" \
+				--reverse \
+				--select-1 \
+				--with-nth "-1" \
+			;
+	)"
+
+	if [[ -d "${worktree_dir}" ]]; then
+		cd "${worktree_dir}" || return 1
+	fi
+}
