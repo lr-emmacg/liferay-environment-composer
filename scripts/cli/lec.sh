a3e4235c3cead8b9807071ef88a97ef4f9b2de6e
Drew Brokke <drew.brokke@liferay.com>
2025-10-06 17:40:53 -0500

    LPD-67313 lec.sh: wording

--- scripts/cli/lec.sh
+++ scripts/cli/lec.sh
@@ -68,7 +68,7 @@ _printHelpAndExit() {
 		  start                            Start a Composer project
 		  stop                             Stop a Composer project
 		  clean                            Stop a Composer project and remove Docker volumes
-		  update                           Check for updates to Composer and lec. Updates to the latest release tag by default; use "--unstable" to update to latest master changes
+		  update [--unstable]              Check for updates to Composer and lec. The "--unstable" flag updates to latest master branch.
 
 		  importDLStructure <sourceDir>    Import a Document Library (file structure only, no content) into configs/common/data/document_library
 
